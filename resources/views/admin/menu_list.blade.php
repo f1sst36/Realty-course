74f4f29bcb362c9af82e17e2ce09d5e20067e292
Maksim <f1sst36@gmail.com>
2020-10-22 17:40:19 +0300

    fixed permissions for menu

--- resources/views/admin/menu_list.blade.php
+++ resources/views/admin/menu_list.blade.php
@@ -1,17 +1,22 @@
 @extends('admin.layout')
 @section('content')
 <div class="pull-right">
+  @if(isset($accesses[4]))
   <a href="{{ route('createMenuForm') }}" class="btn btn-default" title="Добавить">
     <span class="glyphicon glyphicon-plus"></span>
   </a>
+  @endif
+  @if(isset($accesses[5]))
   <button type="button" class="btn btn-default button_delete" title="Удалить">
     <span class="glyphicon glyphicon-trash"></span>
   </button>
+  @endif
 </div>
 
 <h1 class="h3">Все пункты меню</h1>
 <div class="clearfix"></div>
 <hr>
+@if(isset($accesses[6]))
 @if($menus->isNotEmpty())
 <form class="form_delete" method="post" action="{{ route('deleteMenu') }}">
 @csrf
@@ -26,7 +31,11 @@
 @foreach($menus as $menu)
     <tr>
         <td><input type="checkbox" name="menu-{{ $menu->id }}" class="sell_item" value="{{ $menu->id }}"></td>
+        @if(isset($accesses[5]))
         <td><a href="{{ route('editMenuItemForm', $menu->id) }}">{{ $menu->title }}</a></td>
+        @else
+        <td>{{ $menu->title }}</td>
+        @endif
     </tr>
 @endforeach
 </tbody>
@@ -34,6 +43,10 @@
     </form>
 @else
 <p>Список пунктов меню пуст.</p>
+@endif
+
+@else
+<p>Список пунктов меню недоступен.</p>
 @endif
 
 <script>
